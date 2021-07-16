cd68749680fba60856ea9a793b0895c2f1dc6b36
Joe Rinehart <joerinehart91@gmail.com>
2021-07-16 14:48:34 -0400

    commit

--- calendar.js
+++ calendar.js
@@ -1,5 +1,6 @@
 const baseURL = "https://calendarific.com/api/v2/holidays"
 const key = "d8fd5872a88a330080988f3e467a6a8d2319d2b4"
+const listItem = document.getElementById("list-items")
 
 let button1 = document.getElementById("button-fetch")
 button1.addEventListener("click", handleClick)
@@ -7,16 +8,26 @@ button1.addEventListener("click", handleClick)
 function getHoliday(month, year) {
     fetch (`${baseURL}?api_key=${key}&country=US&year=${year}&month=${month}`)
         .then(response => response.json())
-        .then(json => displayData(json));
+        .then(json => displayData(json.response.holidays));
 }
 
-function displayData(holiday) {
-    console.log(holiday)
+function displayData(holidays) {
+    holidays.forEach(holiday => {
+        let li = document.createElement("li");
+        li.innerText = `${holiday.date.iso} - ${holiday.name} - ${holiday.description}`;
+        listItem.appendChild(li)
+    })
+
+    console.log(holidays)
 }
 
 function handleClick() {
     let month = document.getElementById("months").value
     let year = document.getElementById("years").value
+
+    while (listItem.firstChild) {
+        listItem.removeChild(listItem.lastChild)
+    }
 
     getHoliday(month, year)
 }
